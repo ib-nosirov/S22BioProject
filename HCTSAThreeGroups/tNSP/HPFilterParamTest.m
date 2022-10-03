d1fd23818a13f8e3c2a59f0849dade66a52af88f
ib-nosirov <ibrohim@nosirov.com>
2022-10-02 21:35:24 -0600

    perfect match with HCTSA TS_TopFeatures output. Run ParamsNumericalExperiment.m to replicate.

--- HCTSAThreeGroups/tNSP/HPFilterParamTest.m
+++ HCTSAThreeGroups/tNSP/HPFilterParamTest.m
@@ -1,5 +1,5 @@
 function classAcc = HPFilterParamTest(timeSeriesData,keywords,pup,pdown,lag)
-    % The PH Walker funcction achieved 58.03% accuracy on a simple linear
+    % The PH Walker function achieved 55.25% accuracy on a simple linear
     % classifier; my goal is to write this classifier by hand.
     % step 1: compute the lag-1 autocorrelation of every timeseries
     % (feature space mapping).
@@ -7,41 +7,15 @@ function classAcc = HPFilterParamTest(timeSeriesData,keywords,pup,pdown,lag)
     numOfTS = length(timeSeriesData);
     autoCorrData = zeros(numOfTS,1);
     for ii=1:numOfTS
-         y = timeSeriesData{ii};
-         N = length(y)-1;
+         y = normalize(timeSeriesData{ii});
          res = HighPassFilter(y,pup,pdown);
-%          nFFT = 2^(nextpow2(N)+1);
-%          F = fft(res-mean(res),nFFT);
-%          F = F.*conj(F);
-%          acf = ifft(F); % Wiener–Khinchin
-%          acf = acf./acf(1); % Normalize
-%          acf = real(acf);
-%          acf = acf(1:N);
-%          autoCorrData(ii) = acf(lag+1);
          acf = autocorr(double(res));
          autoCorrData(ii) = acf(1+lag);
     end
+    % Same as TS_Compute up until this point.
     % step 2: run discriminant analysis in the feature space.
     groupLabels = categorical(reshape(keywords,[numOfTS,1]));
-    data = table(groupLabels,autoCorrData);
-    mdl = fitcdiscr(data,'groupLabels','Prior','empirical',...
-    'DiscrimType','linear','Weights',InverseProbWeight(groupLabels),'KFold',10);
-    yTrain = groupLabels;
-    yPredict = kfoldPredict(mdl);
-    classLabels = categories([yPredict;yTrain]);
-    classAcc = 100*mean(arrayfun(@(x) mean(yPredict(yTrain==x)==yTrain(yTrain==x)),classLabels));
-
-    function weights = InverseProbWeight(Labels)
-        classNames = unique(Labels);
-        numClasses = length(classNames);
-        numObs = length(Labels);
-        
-        weights = zeros(size(Labels));
-        
-        for x = 1:numClasses
-	        isClass = (Labels == classNames(x));
-	        weights(isClass) = numObs/sum(isClass);
-        end
-    end
+    fnLoss = @(yTest,yPredict) 100*mean(arrayfun(@(x) mean(yPredict(yTest==x)==yTest(yTest==x)),{'direct','random','serial'}));
+    fnTestStat = @(XTrain,yTrain,XTest,yTest) fnLoss(yTest,classify(XTest,XTrain,yTrain,'linear'));
+    classAcc = fnTestStat(autoCorrData,groupLabels,autoCorrData,groupLabels);
 end
-%    fn_handle = @(XTrain,yTrain,XTest,yTest) fn_loss(yTest,classify(XTest,XTrain,yTrain,'linear'));
--- HCTSAThreeGroups/tNSP/ParamsNumericalExperiment.m
+++ HCTSAThreeGroups/tNSP/ParamsNumericalExperiment.m
@@ -1,11 +1,10 @@
 N = 10;
 pup = rand(1,N);
-pdown = 1-pup;
+pdown = 1 - pup;
 lag = 1;
 for ii=1:N
-Copy_of_HPFilterParamTest(timeSeriesData,keywords,pup(ii),pdown(ii),lag)
+    sprintf('pup: %4.2f',pup(ii))
+    sprintf('pdown: %4.2f',pdown(ii))
+    HPFilterParamTest(timeSeriesData,keywords,pup(ii),pdown(ii),lag)
 end
-% the parameters don't seem to make a difference.
-%for ii=1:N
-%    HPFilterParamTest(timeSeriesData,keywords,pup(ii),pdown(ii),lag)
-%end
+HPFilterParamTest(timeSeriesData,keywords,0.6,0.4,lag)
